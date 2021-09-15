b67fcbd8e9a157483d0ec31305c4feb99f62ea3e
Cools <Cools@LAPTOP-1QHU7LDM>
2021-09-16 00:27:15 +0300

    sd

---
+++ test.bll/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.bll
+{
+    public class Class1
+    {
+        public static void testTest()
+        {
+
+            Console.WriteLine("hello");
+        }
+    }
+}
